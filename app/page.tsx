cfac271bfaaaa910dd35e0278f0d55e8d85352ff
muddathir_musa <muddathirmusa@gmail.com>
2025-07-19 10:53:15 +0200

    implement ESlint & prettier

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div>
-      <p>Welcome to your Next.js application!</p>
+      <p className="text-primary-900">Welcome to your Next.js application!</p>
     </div>
   );
 }
