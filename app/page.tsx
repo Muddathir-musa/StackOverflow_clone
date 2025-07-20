286f0917453181179514e39664dcb0efd35b6a8c
muddathir_musa <muddathirmusa@gmail.com>
2025-07-20 10:28:45 +0200

    Assets and MetaData

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,12 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div>
-      <p className="text-primary-900">Welcome to your Next.js application!</p>
+      <p className="text-primary-900 font-inter">
+        Welcome to your Next.js application!
+      </p>
+      <p className="text-primary-900 font-SpaceGrotesk">
+        Welcome to your Next.js application!
+      </p>
     </div>
   );
 }
